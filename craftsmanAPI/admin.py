2bcdf4ab42e978d08ddb2a2df1dd2a65f2af9c19
Patrik Duda <patrikduda94@gmail.com>
2023-11-14 16:18:02 +0100

    Register models on admin site

--- craftsmanAPI/admin.py
+++ craftsmanAPI/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Craftsman, Project, Review, Skill
 
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_at')
 # Register your models here.
+@admin.register(Craftsman)
+class CraftsmanAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'phone')
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'craftsman')
+
+@admin.register(Review)
+class ReviewModel(admin.ModelAdmin):
+    list_display = ('id', 'craftsman', 'author', 'rating')
