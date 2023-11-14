ceb65ccb966f9fc0eae76df9e85a7d65658117a5
Patrik Duda <patrikduda94@gmail.com>
2023-11-14 11:01:58 +0100

    Define Project model

--- craftsmanAPI/models.py
+++ craftsmanAPI/models.py
@@ -24,3 +24,14 @@ class Craftsman(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    craftsman = models.ForeignKey(Craftsman, on_delete=models.CASCADE)
+    project_picture = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
