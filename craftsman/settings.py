6ed906bbff41865951bcb364367fbbba8afcfa9e
Patrik Duda <patrikduda94@gmail.com>
2023-11-15 08:32:01 +0100

    Set up media

--- craftsman/settings.py
+++ craftsman/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from decouple import config
 
@@ -115,6 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Medial folder settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
