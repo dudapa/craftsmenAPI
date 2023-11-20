182fb89210f019fdc45f33b8a00489095c0eb0c3
Patrik Duda <patrikduda94@gmail.com>
2023-11-20 07:54:00 +0100

    Add nested serializer SkillSerializer to CraftsmanSerializer

--- craftsmanAPI/serializers.py
+++ craftsmanAPI/serializers.py
@@ -2,12 +2,13 @@ from rest_framework.serializers import ModelSerializer
 from .models import Craftsman, Project, Review, Skill
 
 
-class Skill(ModelSerializer):
+class SkillSerializer(ModelSerializer):
     class Meta:
         model = Skill
         fields = ['id', 'name', 'created_at', 'updated_at']
     
 class CraftsmanSerializer(ModelSerializer):
+    skills = SkillSerializer(many=True, read_only=True)
     class Meta:
         model = Craftsman
         fields = ['id', 'name', 'email', 'phone', 'address', 'profile_picture', 'skills', 'created_at', 'updated_at']
