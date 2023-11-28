bcfd656f2939a5b0d717c4d18045cafaec05c9ca
Patrik Duda <patrikduda94@gmail.com>
2023-11-28 13:18:12 +0100

    Change plural name of Craftsman model on django admin panel

--- craftsmanAPI/models.py
+++ craftsmanAPI/models.py
@@ -25,6 +25,9 @@ class Craftsman(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        verbose_name_plural = 'craftsmen'
+
     def __str__(self):
         return f'{self.user.first_name} '
     
